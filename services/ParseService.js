85ba89969a169fa10dc256926c15a1c7fefc071a
Vladislav <90531033+molch4nov@users.noreply.github.com>
2024-03-13 17:45:19 +0300

    pre

--- services/ParseService.js
+++ services/ParseService.js
@@ -1,6 +1,8 @@
 import XLSX from 'xlsx'
+import * as fs from "fs";
 
 const workbook = XLSX.readFile('../docs/index.xlsx');
 const sheet_name_list = workbook.SheetNames;
-console.log(sheet_name_list)
-// console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]));
+
+
+fs.writeFileSync('./target.json', JSON.stringify(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[3]])))
